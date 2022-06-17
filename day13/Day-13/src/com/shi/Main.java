ada89dbcd223532f34cabb19bfbe3d01b2b7d8b9
Shivani Gupta <101566441+shivani8210@users.noreply.github.com>
2022-06-17 23:44:22 +0530

    Add files via upload

---
+++ day13/Day-13/src/com/shi/Main.java
@@ -0,0 +1,32 @@
+package com.shi;
+
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+		System.out.println("start of main..");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num1");
+		try {
+			int num1 = sc.nextInt();
+			System.out.println("Enter num2");
+			int num2 = sc.nextInt();
+			int num3 = num1 / num2;
+			if(num3 > 10){
+				System.out.println("Welcome to Exception Handling ");
+			}
+			else {
+				System.out.println("Enter num1 value greater then num2");
+			}
+		}
+		catch(NullPointerException ae) {
+			System.out.println("Enter integer value");
+		}
+		catch(Exception e) {
+			
+		}
+		String message = null;
+		System.out.println("Message is :"+message.toUpperCase());
+		System.out.println("end of main");
+	}
+}
